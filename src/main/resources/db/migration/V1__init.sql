a605f9ffa766c231a91c0fa5a525ea4c6c5a878f
Evgenesis <toy.store.od@gmail.com>
2025-11-11 23:48:32 +0100

    add Entity

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -8,8 +8,8 @@ CREATE TABLE posts (
 );
 
 INSERT INTO posts (title, content, created,  likes) VALUES
-                                                        ('First post','This is content of the first post', CURRENT_TIMESTAMP, 10 ),
-                                                        ('Second post','This is content of the second post', CURRENT_TIMESTAMP, 6 );
+                                                        ('First post','This is content of the first post', CURRENT_TIMESTAMP, 6 ),
+                                                        ('Second post','This is content of the second post', CURRENT_TIMESTAMP, 3 );
 
 
 CREATE SCHEMA IF NOT EXISTS v1_iam_service;
